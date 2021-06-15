2bc492be716a11751de9b291938dd52ec9ced6aa
Zeshan Ayub <unofficialmz@gmail.com>
2021-06-15 17:50:33 +0500

    PhoneBook

---
+++ PhoneBookCrud/PhoneBookCrud/Models/AddContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneBookCrud.Models
+{
+    public class AddContactController : Controller
+    {
+        // GET: AddContact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
